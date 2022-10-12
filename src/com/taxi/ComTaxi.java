cc69019a972c8931403bbee16d15acc3a88f0cab
Stanislav Cheslavsky <stas.hess@gmail.com>
2022-10-12 19:52:31 +0300

    distance service

--- src/com/taxi/ComTaxi.java
+++ src/com/taxi/ComTaxi.java
@@ -5,6 +5,16 @@ import com.taxi.driver.Driver;
 import com.taxi.flow.GeneralFlow;
 import com.taxi.ride.Ride;
 
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ComTaxi {
 
 
---
+++ src/com/taxi/service/DistanceService.java
@@ -0,0 +1,37 @@
+package com.taxi.service;
+
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DistanceService {
+    private static double getDistanceBetween(String city1, String city2) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder(new URI("https://www.distance-cities.com/search?from=" + city1 + "&to=" + city2)).build();
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            Pattern pattern = Pattern.compile("(?<=<span id=\"rkm\">)(.*?)(?=<\\/span>)");
+            Matcher matcher = pattern.matcher(response.body());
+            if (matcher.find())
+            {
+                String doubleValue = matcher.group(1);
+                System.out.println(doubleValue);
+
+                doubleValue = doubleValue.replaceAll("−", "-");
+                doubleValue = doubleValue.replaceAll("[^-.,0-9]", "");
+
+                DecimalFormat df = new DecimalFormat();
+                System.out.println(doubleValue);
+                return df.parse(doubleValue).doubleValue();
+            }
+        } catch (Exception e){
+        }
+        return 0;
+    }
+}
